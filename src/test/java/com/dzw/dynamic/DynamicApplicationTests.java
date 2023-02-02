903487337e525de98ba10f70c6ec28c2f3b60160
刘怡兵 <liuyibing@edaijia-inc.cn>
2023-02-02 21:40:16 +0800

    初始化

---
+++ src/test/java/com/dzw/dynamic/DynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzw.dynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
